682a648ee2475e08d62404003b80f94218a5c601
manuelsantiengineering <manuelsantiengineering@gmail.com>
2017-11-07 22:40:24 -0500

    Added the recipe update to the recipe-detail component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 import { Recipe } from '../recipe.model';
 
@@ -8,14 +8,23 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() selectedRecipe = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
     new Recipe('Test Recipe 1', 'Test Description 1', 'https://get.pxhere.com/photo/dish-meal-food-green-mediterranean-vegetable-recipe-healthy-snack-cuisine-food-photography-zucchini-vegetarian-food-delicious-food-rosemary-appetizer-plated-food-beautiful-food-zucchini-wraps-zucchini-slices-fish-fillet-recipe-fish-recipes-1376204.jpg' ),
     new Recipe('Test Recipe 2', 'Test Description 2', 'https://get.pxhere.com/photo/dish-meal-food-green-mediterranean-vegetable-recipe-healthy-snack-cuisine-food-photography-zucchini-vegetarian-food-delicious-food-rosemary-appetizer-plated-food-beautiful-food-zucchini-wraps-zucchini-slices-fish-fillet-recipe-fish-recipes-1376204.jpg' )
   ];
 
+  loadedRecipe: Recipe;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSelectedRecipe(recipe: Recipe){
+    this.loadedRecipe = recipe;
+    this.selectedRecipe.emit(recipe);
   }
 
 }
--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from './recipe.model';
+
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
   styleUrls: ['./recipes.component.css']
 })
 export class RecipesComponent implements OnInit {
+  loadedRecipe: Recipe;
+  loadRecipe: boolean;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onDisplayRecipe(selectedRecipe: Recipe){
+    this.loadedRecipe = selectedRecipe;
+    this.loadRecipe = true;
   }
 
 }
